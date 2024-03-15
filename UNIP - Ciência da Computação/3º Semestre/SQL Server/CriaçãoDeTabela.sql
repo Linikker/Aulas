fe653c767bf1cb0813e4242f58873fa61ada66ce
Gustavo Liniker <132775148+Linikker@users.noreply.github.com>
2024-03-14 21:17:22 -0300

    Parte 1_CriaçãoDeTabela.sql

--- UNIP - Ciência da Computação/3º Semestre/SQL Server/CriaçãoDeTabela.sql
+++ UNIP - Ciência da Computação/3º Semestre/SQL Server/CriaçãoDeTabela.sql
@@ -1,1 +1,73 @@
+--Criando e Manipulando Tabelas em SQL.
 
+
+--Criar Tabela:
+
+  create table pessoas
+    (
+    	idPessoa int primary key identity(1,1),
+    	nome varchar(50),
+    	rg varchar(10),
+    	cpf varchar(12)
+    )
+    go
+
+
+--Pesquisar Tabelas:
+  
+  Select * from pessoas
+
+
+--Inserir valores de forma simples:
+  
+  insert into pessoas
+  values
+  ('  Nome  ','  CPF  ','  RG  ')
+
+
+
+--Inserir valores de ordenadamente:
+
+  insert into pessoas(nome, rg, cpf)
+  values
+  ('  Nome  ','  CPF  ','  RG  '),
+  ('  Nome  ','  CPF  ','  RG  '),
+  ('  Nome  ','  CPF  ','  RG  ')
+
+
+--Deletando Valores da Tabela ??
+
+
+
+--Ordenando Dados :
+  
+  select * from pessoas
+  order by nome asc --Crescente(Padrão)
+  order by nome desc --Decrescente
+
+
+-- Pesquisa especifica(Primeiro da Lista ordenado pelo NOME e RG):
+
+  Select top 1 * from pessoas
+  where nome = 'Manoel'
+  order by rg
+
+
+-- Pesquisa por trechos:
+
+  Select top 1 * from pessoas
+  where nome like 'M%' -- Primeira Letra
+  
+  where nome like '__N%'-- 3ª Letra
+  
+  where nome like '%L' -- Termina com o caracter especificado
+    
+  
+-- Lista todos os valores em maiusculo:
+
+  select UPPER(nome) from pessoas
+
+
+-- Mostrar valores sem repetição:
+
+	select distinct(nome) from pessoas
