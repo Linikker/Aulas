7774416f54f92db05408fc5bf8e6c0e6b0e7cd07
Gustavo Liniker <132775148+Linikker@users.noreply.github.com>
2024-08-27 16:52:55 -0300

    Atualização do Semestre
    Adicionada a matéria e aula sobre PEE

---
+++ UNIP - Ciência da Computação/4º Semestre/Aplic. Program. Orientada Objeto/AulaPEE/CrudPessoas/src/main/java/DAL/PessoaDAO.java
@@ -0,0 +1,23 @@
+package DAL;
+
+import Modelo.Pessoa;
+
+public class PessoaDAO
+{
+    public void cadastrarPessoa(Pessoa pessoa)
+    {
+    }
+
+    public Pessoa pesquisarPessoaPorID(Pessoa pessoa)
+    {
+        return null;
+    }
+
+    public void editarPessoa(Pessoa pessoa)
+    {
+    }
+
+    public void excluirPessoa(Pessoa pessoa)
+    {
+    }
+}
---
+++ UNIP - Ciência da Computação/4º Semestre/Aplic. Program. Orientada Objeto/AulaPEE/CrudPessoas/src/main/java/Modelo/Pessoa.java
@@ -0,0 +1,54 @@
+package Modelo;
+
+public class Pessoa
+{
+    private int id;
+    private String Nome;
+    private String RG;
+    private String CPF;
+
+    public int getId()
+        {
+        return id;
+        }
+
+    public void setId(int id)
+        {
+        this.id = id;
+        }
+
+    public String getNome()
+        {
+        return Nome;
+        }
+
+    public void setNome(String Nome)
+        {
+        this.Nome = Nome;
+        }
+
+    public String getRG()
+        {
+        return RG;
+        }
+
+    public void setRG(String RG)
+        {
+        this.RG = RG;
+        }
+
+    public String getCPF()
+        {
+        return CPF;
+        }
+
+    public void setCPF(String CPF)
+        {
+        this.CPF = CPF;
+        }
+    
+    
+    
+    
+    
+}
