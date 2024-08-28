acdf75d435ac09571bd9c8db1d0d9a433a36d12d
Gustavo Liniker <132775148+Linikker@users.noreply.github.com>
2024-08-28 09:16:41 -0300

    Atualização do semestre

---
+++ UNIP - Ciência da Computação/4º Semestre/Aplic. Program. Orientada Objeto/AulaPEE/CrudPessoas/src/main/java/com/mycompany/aulapee/AulaPEE.java
@@ -0,0 +1,13 @@
+package com.mycompany.aulapee;
+
+import Apresentacao.frmPrincipal;
+
+public class AulaPEE
+{
+
+    public static void main(String[] args)
+        {
+            frmPrincipal frmP = new frmPrincipal(null, true);
+            frmP.setVisible(true);
+        }
+}
